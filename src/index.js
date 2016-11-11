f905c91615bacda5a6e46f6b7a6746ed24c8f34b
antigvozd <antigvozd@yandex.ru>
2016-11-12 01:49:42 +0300

    sdfsf

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
+<<<<<<< HEAD
 import canonize from './customize';
+=======
+>>>>>>> parent of 6be41fe... This is a new commit for what I originally planned to be amended
 import cors from 'cors';
 
 const app = express();
@@ -10,9 +13,15 @@ app.get('/', (req, res) => {
   });
 });
 
+<<<<<<< HEAD
 app.get('/task2b', (req, res) => {
 	const username = req.query.username != undefined ? canonize(req.query.username) : 'Invalid username';
 	res.send(username);
+=======
+app.get('/task2a', (req, res) => {
+	const sum = (+req.query.a || 0) + (+req.query.b || 0);
+	res.send(sum.toString());
+>>>>>>> parent of 6be41fe... This is a new commit for what I originally planned to be amended
 });
 
 app.listen(3000, () => {
